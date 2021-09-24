f63d117e78acde1c72dc59c230b836ae87031c6c
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2021-09-23 23:27:37 -0500

    added adios2 VOL in vol-tests.

--- packages/hdf5-vol-tests/package.py
+++ packages/hdf5-vol-tests/package.py
@@ -14,21 +14,34 @@ class Hdf5VolTests(CMakePackage):
 
     maintainers = ['hyoklee']
 
-    version('master', preferred=True)
-    variant('vol-async', default=True, description='Enable async VOL')
+    version('master', commit='9a147d3')
+    version('hyoklee.master',
+            git='https://github.com/hyoklee/vol-tests', preferred=True)
+    version('local.master', 
+            git='file:///Users/hyoklee/src/vol-tests')
+    variant('vol-async', default=False, description='Enable async VOL')
     variant('vol-cache', default=False, description='Enable cache VOL')
     variant('vol-external-passthrough', default=False, 
             description='Enable external pass-through VOL')
+    variant('vol-log', default=False, 
+            description='Enable log-based VOL')
+    variant('vol-adios2', default=False, 
+            description='Enable ADIOS2 VOL')
+    variant('vol-rest', default=False, 
+            description='Enable REST VOL')
 
     variant('async', default=True, description='Enable parallel tests.')
     variant('parallel', default=True, description='Enable async API tests.')
     variant('part', default=True, 
             description='Enable building the main test executable.')
-    depends_on('szip', when='+parallel')
+    # depends_on('szip')
     depends_on('hdf5-vol-async', when='+vol-async')
     depends_on('hdf5-vol-cache', when='+vol-cache')
     depends_on('hdf5-vol-external-passthrough', 
                when='+vol-external-passthrough')
+    depends_on('hdf5-vol-log', when='+vol-log')
+    depends_on('adios2+shared+hdf5 ^hdf5@1.12.1', when='+vol-adios2')
+    depends_on('hdf5-vol-rest', when='+vol-rest')    
 
     def cmake_args(self):
         args = []
